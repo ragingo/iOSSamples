2c98a77ca0d18f7e9b3439c4a7596c1698045827
ragingo <save.the.world.crush@gmail.com>
2021-03-13 00:09:51 +0900

    プロジェクト初期状態をコミット

---
+++ Smaple001-VideoPlayer/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by ragingo on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
