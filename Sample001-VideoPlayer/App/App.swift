7fc487b3babf753a8e10ed85c3afc1dfa5ff61f9
ragingo <save.the.world.crush@gmail.com>
2021-06-01 21:46:42 +0900

    SwiftUI & iOS14 の最小コードが動くよう変更

---
+++ Sample001-VideoPlayer/App/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  App
+//
+//  Created by ragingo on 2021/06/01.
+//
+
+import SwiftUI
+
+@main
+struct App: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+            Button("aaa") {
+                print("click!")
+            }
+        }
+    }
+}
