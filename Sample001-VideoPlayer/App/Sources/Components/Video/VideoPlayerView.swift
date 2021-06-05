522885f356b0e635639a9ffb87216d58ee7e9a3a
ragingo <save.the.world.crush@gmail.com>
2021-06-06 00:20:50 +0900

    [Sample001-VideoPlayer] ProgressView 表示

--- Sample001-VideoPlayer/App/Sources/Components/Video/VideoPlayerView.swift
+++ Sample001-VideoPlayer/App/Sources/Components/Video/VideoPlayerView.swift
@@ -10,24 +10,35 @@ import SwiftUI
 // プレーヤ
 struct VideoPlayerView: View {
     private let player: VideoPlayerProtocol
+    @State private var isReady = false
 
     init(player: VideoPlayerProtocol = VideoPlayer()) {
         self.player = player
     }
 
     var body: some View {
-        VStack {
-            VideoSurfaceView(playerLayer: player.layer)
-                .padding(.horizontal, 8)
-            VideoControllerView(player: player)
-                .padding(.horizontal, 8)
-                .padding(.vertical, 4)
+        ZStack {
+            VStack {
+                VideoSurfaceView(playerLayer: player.layer)
+                    .padding(.horizontal, 8)
+                VideoControllerView(player: player)
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 4)
+            }
+            if !isReady {
+                ProgressView()
+            }
         }
         .onAppear {
             player.prepare()
         }
         .onDisappear {
             player.invalidate()
+        }
+        .onReceive(player.statusSubject) { status in
+            if status == .readyToPlay {
+                isReady = true
+            }
         }
     }
 
