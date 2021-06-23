af833d9ae1e87e755e6b1990696e969befb1f0e6
ragingo <save.the.world.crush@gmail.com>
2021-06-24 00:27:44 +0900

    [Sample003-Metal] テクスチャ描画

--- Sample003-Metal/App/Shaders.metal
+++ Sample003-Metal/App/Shaders.metal
@@ -15,17 +15,23 @@ using namespace metal;
 struct ColorInOut
 {
     float4 position [[ position ]];
+    float2 texCoords;
 };
 
 vertex ColorInOut default_vs(constant float4 *positions [[ buffer(0) ]],
+                             constant float2 *texCoords [[ buffer(1) ]],
                              uint vid [[ vertex_id ]])
 {
     ColorInOut out;
     out.position = positions[vid];
+    out.texCoords = texCoords[vid];
     return out;
 }
 
-fragment float4 default_fs(ColorInOut in [[ stage_in ]])
+fragment float4 default_fs(ColorInOut in [[ stage_in ]],
+                           texture2d<float> texture [[ texture(0) ]])
 {
-    return float4(1, 0, 0, 1);
+    constexpr sampler colorSampler;
+    float4 color = texture.sample(colorSampler, in.texCoords);
+    return color;
 }
--- Sample003-Metal/App/Sources/MainViewController.swift
+++ Sample003-Metal/App/Sources/MainViewController.swift
@@ -10,8 +10,9 @@ import MetalKit
 
 class MainViewController: UIViewController, MTKViewDelegate {
     private var device: MTLDevice!
-    private var renderPipelineState: MTLRenderPipelineState!
+    private var renderPipelineState: MTLRenderPipelineState?
     private var commandQueue: MTLCommandQueue!
+    private var texture: MTLTexture?
     private var renderPassDescriptor = MTLRenderPassDescriptor()
     private let vertices: [Float] = [
         -1, -1, 0, 1,
@@ -19,7 +20,15 @@ class MainViewController: UIViewController, MTKViewDelegate {
         -1, 1, 0, 1,
         1, 1, 0, 1
     ]
+    private let texCoords: [Float] = [
+        0, 1,
+        1, 1,
+        0, 0,
+        1, 0
+    ]
+
     private var vertexBuffer: MTLBuffer?
+    private var texCoordsBuffer: MTLBuffer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,15 +40,13 @@ class MainViewController: UIViewController, MTKViewDelegate {
         self.device = device
         self.commandQueue = device.makeCommandQueue()
 
-        if !prepare() {
-            return
-        }
-
         let mtkView = MTKView(frame: view.layer.frame, device: device)
         view.addSubview(mtkView)
         mtkView.delegate = self
         mtkView.enableSetNeedsDisplay = true
         mtkView.framebufferOnly = false
+
+        loadTexture()
     }
 
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -54,6 +61,8 @@ class MainViewController: UIViewController, MTKViewDelegate {
             return
         }
 
+        guard let texture = texture else { return }
+
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
 
         guard let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor) else {
@@ -64,9 +73,29 @@ class MainViewController: UIViewController, MTKViewDelegate {
             let size = vertices.count * MemoryLayout<Float>.size
             vertexBuffer = device.makeBuffer(bytes: vertices, length: size)
         }
+        if texCoordsBuffer == nil {
+            let size = texCoords.count * MemoryLayout<Float>.size
+            texCoordsBuffer = device.makeBuffer(bytes: texCoords, length: size)
+        }
+        if renderPipelineState == nil {
+            guard let library = device.makeDefaultLibrary() else { return }
+            let renderDesc = MTLRenderPipelineDescriptor()
+            renderDesc.vertexFunction = library.makeFunction(name: "default_vs")
+            renderDesc.fragmentFunction = library.makeFunction(name: "default_fs")
+            renderDesc.colorAttachments[0].pixelFormat = texture.pixelFormat
+
+            guard let renderPipelineState = try? device.makeRenderPipelineState(descriptor: renderDesc) else {
+                return
+            }
+            self.renderPipelineState = renderPipelineState
+        }
+
+        guard let renderPipelineState = renderPipelineState else { return }
 
         renderEncoder.setRenderPipelineState(renderPipelineState)
         renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.setVertexBuffer(texCoordsBuffer, offset: 0, index: 1)
+        renderEncoder.setFragmentTexture(texture, index: 0)
         renderEncoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
         renderEncoder.endEncoding()
 
@@ -75,18 +104,40 @@ class MainViewController: UIViewController, MTKViewDelegate {
         commandBuffer.waitUntilCompleted()
     }
 
-    private func prepare() -> Bool {
-        guard let library = device.makeDefaultLibrary() else { return false }
-        let renderDesc = MTLRenderPipelineDescriptor()
-        renderDesc.vertexFunction = library.makeFunction(name: "default_vs")
-        renderDesc.fragmentFunction = library.makeFunction(name: "default_fs")
-        renderDesc.colorAttachments[0].pixelFormat = .bgra8Unorm
-
-        guard let renderPipelineState = try? device.makeRenderPipelineState(descriptor: renderDesc) else {
-            return false
+    private func loadTexture() {
+        let urlString = "https://www.pakutaso.com/shared/img/thumb/sakura430-0_TP_V4.jpg"
+        guard let url = URL(string: urlString) else {
+            return
         }
-        self.renderPipelineState = renderPipelineState
 
-        return true
+        var request = URLRequest(url: url)
+        request.setValue("ios_metal_test", forHTTPHeaderField: "UserAgent")
+
+        let task = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            guard error == nil else { return }
+            guard
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200 else {
+                return
+            }
+            guard let data = data else { return }
+
+            let textureLoader = MTKTextureLoader(device: self.device)
+            textureLoader.newTexture(data: data, options: nil) { (texture, error) in
+                guard error == nil else { return }
+                guard let texture = texture else { return }
+                self.texture = texture
+
+                DispatchQueue.main.async {
+                    guard let mtkView = self.view.subviews.first as? MTKView else {
+                        return
+                    }
+                    mtkView.colorPixelFormat = texture.pixelFormat
+                    mtkView.setNeedsDisplay()
+                }
+            }
+        }
+        task.resume()
     }
 }
