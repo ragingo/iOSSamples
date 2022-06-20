a197b34628468c086d7b7cd1b7b16090f96ff448
ragingo <save.the.world.crush@gmail.com>
2022-06-20 19:29:51 +0900

    [Sample005-Charts] 目盛りを引いてみた

--- Sample005-Charts/Shared/ContentView.swift
+++ Sample005-Charts/Shared/ContentView.swift
@@ -25,22 +25,44 @@ struct ContentView: View {
     }
 
     // 雑な棒グラフ(横向き)
+    @ViewBuilder
     private func makeHorizontalBarChart(geo: GeometryProxy, maxValue: Int) -> some View {
-        LazyVStack(alignment: .leading, spacing: 10) {
-            ForEach(makeShuffledData(range: 1..<100, count: 5), id: \.self) { item in
-                let length = geo.size.width / CGFloat(maxValue) * CGFloat(item)
-                HStack {
-                    Rectangle()
-                        .frame(width: CGFloat(length), height: 30)
-                        .overlay(
-                            LinearGradient(colors: [.blue, .green], startPoint: .leading, endPoint: .trailing)
-                        )
-                        .cornerRadius(8)
-                    Text("\(item)")
+        let items = makeShuffledData(range: 1..<100, count: 5)
+        let barHeight = 30.0
+        let barSpacing = 10.0
+        let majorGuidelineCount = 10
+
+        ZStack {
+            HStack {
+                ForEach(0..<majorGuidelineCount, id: \.self) { index in
+                    Spacer()
+                        .frame(width: geo.size.width / CGFloat(majorGuidelineCount))
+                    Divider()
+                        .border(Color.gray, width: 0.1)
                 }
+                Spacer()
             }
+
+            VStack(alignment: .leading, spacing: barSpacing) {
+                ForEach(items, id: \.self) { item in
+                    let length = geo.size.width / CGFloat(maxValue) * CGFloat(item)
+                    HStack {
+                        Rectangle()
+                            .frame(width: CGFloat(length), height: barHeight)
+                            .overlay(
+                                LinearGradient(colors: [.blue, .green], startPoint: .leading, endPoint: .trailing)
+                            )
+                            .cornerRadius(4)
+                    }
+                    .overlay(
+                        Text("\(item)")
+                    )
+                }
+                Spacer(minLength: 20)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .background(Color.yellow.opacity(0.2))
         }
-        .background(Color.yellow)
     }
 
     // 雑な棒グラフ(縦向き)
