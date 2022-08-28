be56596cf89be2dd337371a144e95d1e68ddad01
ragingo <save.the.world.crush@gmail.com>
2022-08-29 00:04:34 +0900

    [Sample007-ImageProcessing] アセット画像を分割して SwiftUI で表示してみる

---
+++ Sample007-ImageProcessing/Sample007-ImageProcessing/Sample007_ImageProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample007_ImageProcessingApp.swift
+//  Sample007-ImageProcessing
+//
+//  Created by ragingo on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct Sample007_ImageProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
