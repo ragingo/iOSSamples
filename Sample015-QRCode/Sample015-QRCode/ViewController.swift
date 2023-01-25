cef4c6aa796e15022e50e6abe6253ba9dcda9e74
ragingo <save.the.world.crush@gmail.com>
2023-01-25 23:01:15 +0900

    [Sample015-QRCode] initial commit

---
+++ Sample015-QRCode/Sample015-QRCode/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Sample015-QRCode
+//
+//  Created by ragingo on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
