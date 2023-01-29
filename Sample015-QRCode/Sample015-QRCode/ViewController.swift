f542ecf09fcc9cbf94d8105d7c3ea0070ad9c8a0
ragingo <save.the.world.crush@gmail.com>
2023-01-29 22:42:31 +0900

    [Sample015-QRCode] QRコードから取得したデータを画面に表示

--- Sample015-QRCode/Sample015-QRCode/ViewController.swift
+++ Sample015-QRCode/Sample015-QRCode/ViewController.swift
@@ -18,19 +18,32 @@ class ViewController: UIViewController {
         qrCodeReaderView = QRCodeReaderView()
         qrCodeReaderView.translatesAutoresizingMaskIntoConstraints = false
         qrCodeReaderView.layer.borderWidth = 1
-        qrCodeReaderView.layer.borderColor = UIColor.white.cgColor
+        qrCodeReaderView.layer.borderColor = UIColor.gray.cgColor
         view.addSubview(qrCodeReaderView)
 
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = false
+        textView.text = "読み取ったデータはここに表示されます"
+        view.addSubview(textView)
+
         NSLayoutConstraint.activate([
+            // qrCodeReaderView
             qrCodeReaderView.widthAnchor.constraint(equalToConstant: 300),
             qrCodeReaderView.heightAnchor.constraint(equalToConstant: 300),
             view.centerXAnchor.constraint(equalTo: qrCodeReaderView.centerXAnchor),
-            view.centerYAnchor.constraint(equalTo: qrCodeReaderView.centerYAnchor)
+            view.centerYAnchor.constraint(equalTo: qrCodeReaderView.centerYAnchor),
+            // textView
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textView.topAnchor.constraint(equalTo: qrCodeReaderView.bottomAnchor, constant: 16),
+            textView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
         ])
 
         qrCodeReaderView.result
-            .sink(receiveValue: { value in
-                print(value)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak textView] value in
+                textView?.text = value
             })
             .store(in: &cancellables)
 
