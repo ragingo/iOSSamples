fd7559b5e592acee04651355c4fc9536d91dbd8a
ragingo <save.the.world.crush@gmail.com>
2023-07-02 00:15:03 +0900

    [Sample016-Xcode15-iOS17-Experimentation] #upperCase は単一の文字列リテラルのみが与えられるよう修正

---
+++ Sample016-Xcode15-iOS17-Experimentation/MyMacro/Sources/MyMacroMacros/MyMacroError.swift
@@ -0,0 +1,11 @@
+//
+//  MyMacroError.swift
+//
+//
+//  Created by ragingo on 2023/07/01.
+//
+
+import Foundation
+
+public struct MyMacroError: Error {
+}
--- Sample016-Xcode15-iOS17-Experimentation/MyMacro/Sources/MyMacroMacros/UpperCaseMacro.swift
+++ Sample016-Xcode15-iOS17-Experimentation/MyMacro/Sources/MyMacroMacros/UpperCaseMacro.swift
@@ -10,14 +10,37 @@ import SwiftSyntax
 import SwiftSyntaxBuilder
 import SwiftSyntaxMacros
 
+/// 大文字に変換するマクロ
+///
+/// 文字列リテラルの引数が1つでないとコンパイルエラーになるようにしている。
+///
+/// o: #upperCase("abc")
+/// x: #upperCase("abc" + "def")
 public struct UpperCaseMacro: ExpressionMacro {
+    public struct InvalidArgumentError: Error {}
+
     public static func expansion(
         of node: some FreestandingMacroExpansionSyntax,
         in context: some MacroExpansionContext
     ) throws -> ExprSyntax {
-        guard let argument = node.argumentList.first?.expression else {
+        guard let expression = node.argumentList.first?.expression else {
             fatalError("compiler bug")
         }
-        return "\(literal: argument.description.uppercased())"
+
+        guard let stringLiteral = expression.as(StringLiteralExprSyntax.self) else {
+            throw InvalidArgumentError()
+        }
+
+        if stringLiteral.segments.count != 1 {
+            throw InvalidArgumentError()
+        }
+
+        guard case .stringSegment(let segment) = stringLiteral.segments.first else {
+            throw InvalidArgumentError()
+        }
+
+        let text = segment.content.text
+
+        return "\(literal: text.uppercased())"
     }
 }
